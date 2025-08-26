8b204009659e31ab7cf55c4fa42629a70bb32aeb
manasvi001 <manasvi.d007@gmail.com>
2025-08-26 17:45:14 +0530

    v1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import Faq from "./sections/Faq";
-import Features from "./sections/Features";
-import Header from "./sections/Header";
-import Hero from "./sections/Hero";
-import Pricing from "./sections/Pricing";
-
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      <Header/>
+      <Header />
       <Hero />
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,30 +1,32 @@
 import { Element } from "react-scroll";
-import { details, features } from "../constants";
-import Button from "../components/Button";
+
+import { details, features } from "../constants/index.jsx";
+import Button from "../components/Button.jsx";
 
 const Features = () => {
   return (
     <section>
       <Element name="features">
         <div className="container">
-          <div className="relative flex md:flex-wrap fle-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
+          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map(({ id, icon, caption, title, text, button }) => (
               <div
-                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
                 key={id}
+                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
               >
                 <div className="w-full flex justify-start items-start">
                   <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
                     <div className="w-0.5 h-16 bg-s3" />
+
                     <img
                       src={icon}
-                      alt={title}
                       className="size-28 object-contain"
+                      alt={title}
                     />
                   </div>
                 </div>
 
-                <div className="caption mb-5 max-md:mb-6">{caption}</div>
+                <p className="caption mb-5 max-md:mb-6">{caption}</p>
                 <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
                   {title}
                 </h2>
@@ -48,7 +50,9 @@ const Features = () => {
                     />
                   </div>
 
-                  <h3 className="relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase">{title}</h3>
+                  <h3 className="relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase">
+                    {title}
+                  </h3>
                 </li>
               ))}
             </ul>
@@ -58,5 +62,4 @@ const Features = () => {
     </section>
   );
 };
-
 export default Features;
